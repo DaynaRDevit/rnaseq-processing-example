760eadf82b401158584d2a286c440f8ebd048b98
DaynaRDevit <dayna.ride@gmail.com>
2026-07-04 20:44:07 +0100

    Update and rename NGS_Data_Ananlysis(1).R to 1_NGS_Dif_Exp_An.R

--- 1_NGS_Dif_Exp_An.R
+++ 1_NGS_Dif_Exp_An.R
@@ -4,20 +4,32 @@ BiocManager::install("DESeq2")
 
 library(DESeq2)
 
-setwd("C:/Users/sheri/Desktop/R/NGS")
+setwd("your folder pathway")
 
-# Read counts (genes x samples)
-counts <- read.csv("counts.csv", row.names = 1)
-
-# Read metadata (samples x conditions)
-metadata <- read.csv("metadata.csv", row.names = 1)
+ASDvsTD <- read.table("ASDvsTD_CountsF.txt", row.names = 1, header = TRUE)
+metadata <- read.table("metadata.txt", row.names = 1, header = TRUE)
 
 # Ensure sample names match
-all(colnames(counts) == rownames(metadata))
+all(colnames(ASDvsTD) == rownames(metadata))
 metadata$type <- factor(metadata$type)
-dds <- DESeqDataSetFromMatrix(countData = counts,
+dds <- DESeqDataSetFromMatrix(countData = ASDvsTD,
                               colData = metadata,
                               design = ~ type)  # replace 'condition' with your variable
+
+dds <- DESeq(dds)
+res <- results(dds)
+
+# Summary
+
+# Order by adjusted p-value
+resOrdered <- res[order(res$padj), ]
+#write.csv(as.data.frame(resOrdered), 
+#          file = "Dseq2_results_all.csv")
+
+# Significant genes
+sigGenes <- subset(resOrdered, padj < 0.05 & abs(log2FoldChange) > 2)
+#write.csv(as.data.frame(sigGenes), 
+ #         file = "Dseq2_significant.csv")
 dds <- DESeq(dds)
 res <- results(dds)
 # Summary
